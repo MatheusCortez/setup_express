7ba76cad79d78c4971696af641db26e6b5babb93
Matheus Cortez <matheus.cortez@live.com>
2023-07-15 17:43:31 -0300

    feat: add express and hello sended

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-const message = "a";
+import * as express from "express"
+import { Request, Response } from "express"
+
+const app = express()
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello ")
+})
+app.listen(3000, () => console.log("porta 3000"))
